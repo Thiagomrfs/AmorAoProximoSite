ce90e13c7c1de891d2aafa4eb02538dfb0d29931
Thiago <thiagomarbsn@gmail.com>
2021-07-06 15:21:46 -0300

    added update model

--- updates/admin.py
+++ updates/admin.py
@@ -1,3 +1,4 @@
+from updates.models import Update
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Update)
--- updates/models.py
+++ updates/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Update(models.Model):
+    titulo = models.CharField(max_length=100)
+    data = models.DateTimeField()
+    descricao = models.TextField()
