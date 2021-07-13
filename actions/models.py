6323d08c76302991b221ced59a4c0cf156657bd1
Thiago <thiagomarbsn@gmail.com>
2021-07-13 15:13:17 -0300

    added education category

--- actions/models.py
+++ actions/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 class Action(models.Model):
     categories = [
+        ('EDU', 'Educação'),
         ('ESP', 'Esporte'),
         ('CLT', 'Cultura'),
         ('CNM', 'Comida na mesa')
     ]
 
     name = models.CharField(max_length=50)
-    category = models.CharField(max_length=3, choices=categories, default='CLT')
+    category = models.CharField(max_length=3, choices=categories, default='EDU')
     start_date = models.DateField()
     end_date = models.DateField()
     responsible = models.CharField(max_length=50)
