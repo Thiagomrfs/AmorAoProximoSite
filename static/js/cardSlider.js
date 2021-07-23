92b11815cfbc9cdf43a19d43a346f017e0502044
Thiago <thiagomarbsn@gmail.com>
2021-07-23 18:04:14 -0300

    created landing page

--- static/js/cardSlider.js
+++ static/js/cardSlider.js
@@ -22,3 +22,14 @@ $(document).ready(function() {
         } 
     });  
     });
+
+var nav = document.getElementById('nav');
+window.addEventListener("scroll", function(event) {
+    nav.style.transition = "700ms"
+    if(window.pageYOffset>100){
+        nav.style.background = "var(--primary)";
+    }
+    else{
+        nav.style.background = "transparent";
+    }
+});
