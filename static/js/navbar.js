5b9b3ad646dd51d577e8e79036f2ffac347fc4ad
Thiago <thiagomarbsn@gmail.com>
2021-07-30 20:34:28 -0300

    navbar shadow

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -3,8 +3,10 @@ window.addEventListener("scroll", function(event) {
     nav.style.transition = "700ms"
     if(window.pageYOffset>100){
         nav.style.background = "var(--primary)";
+        nav.style.boxShadow = "-8px 5px 10px rgba(0, 0, 0, 0.25)";
     }
     else{
         nav.style.background = "transparent";
+        nav.style.boxShadow = "";
     }
 });
