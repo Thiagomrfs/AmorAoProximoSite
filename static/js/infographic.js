93b7a81982b6c41ccd798246d06ea71ec2ce091d
Thiago <thiagomarbsn@gmail.com>
2021-08-16 16:27:38 -0300

    support text

--- static/js/infographic.js
+++ static/js/infographic.js
@@ -1,9 +1,19 @@
 function updateInfographic() {
     let value = parseFloat(document.querySelector(".donation-indicator").value).toFixed(2);
+    let desc = document.querySelector("#inf-description");
     let totalPrice = 30;
 
-    let percentage = 100*value / totalPrice
+    let percentage = 100*value / totalPrice;
 
-    let info = document.querySelector("#inf-full")
-    info.style.clipPath = `inset(${100 - percentage}% 0px 0px 0px)`
+    let info = document.querySelector("#inf-full");
+
+    info.style.clipPath = `inset(${100 - percentage}% 0px 0px 0px)`;
+
+    if (value > 30)
+    {
+        let quantity = value/30
+        desc.innerHTML = `Com a sua doação conseguimos arrecadar<br />cerca de <b>${quantity.toFixed(0)}</b> cestas básicas!`
+    }
+    else
+        desc.innerHTML = `Com a sua doação conseguimos o equivalente<br/>a <b>${percentage.toFixed(0)}%</b> de uma cesta básica!`
 }
